1e45165dfb9205d8a3e9f5d765272b632d3236aa
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-07 14:28:04 -0300

    feat: requisito 7 completo

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 import UserController from '../controller/user.controller';
 import ValidateLogin from '../middlewares/validateLogin';
+import ValidateUser from '../middlewares/validateUser';
 
 const router = Router();
 const validateLogin = new ValidateLogin();
+const validateUser = new ValidateUser();
 const userController = new UserController();
 
-router.post('/users', userController.create);
+router.post(
+  '/users',
+  validateUser.validateName,
+  validateUser.validateClasse,
+  validateUser.validateLevel,
+  validateUser.validatePassword,
+  userController.create,
+);
 router.post('/login', validateLogin.validateLogin, userController.login);
 export default router;
