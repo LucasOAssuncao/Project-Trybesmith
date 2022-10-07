1b339b3ffe331b695e22d32c7aa61496de5591da
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-07 14:15:25 -0300

    feat: requisito 6 completo

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controller/product.controller';
+import ValidateProduct from '../middlewares/validateProduct';
 
 const router = Router();
 const productController = new ProductController();
+const validateProduct = new ValidateProduct();
 
 router.get('/products', productController.getAll);
 
-router.post('/products', productController.create);
+router.post(
+  '/products',
+  validateProduct.validateName,
+  validateProduct.validateAmount,
+  productController.create,
+);
 
 export default router;
